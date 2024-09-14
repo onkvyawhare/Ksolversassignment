a5e83edd2cab7518c1fe12d6477a5346c9eec2aa
onkvyawhare <onkvyawhare@gmail.com>
2024-09-14 16:41:39 +0530

    deployerrorfix3

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = "http://localhost:8000/api";
+const API_URI = "https://ksolversassignment.onrender.com/api";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 
--- server/Index.js
+++ server/Index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 dbConnection();
 
-//mongodbatlas
+//atlasdb
 
 const PORT = process.env.PORT || 9000;
 
